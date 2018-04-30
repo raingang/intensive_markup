cde6b7d0d232495cbfc0bd96f11d2e2bd019614a
AntonAbrosymov <raingang4@gmail.com>
2018-04-30 17:45:13 +0300

    master Add styles

--- script.js
+++ script.js
@@ -2,13 +2,12 @@ console.log('hello')
 const footerElem = document.getElementById('footer')
 const headerElem = document.getElementById('header')
 window.onscroll = (e) => {
-    console.log(document.body.getBoundingClientRect().top)
-    if(headerElem.getBoundingClientRect().bottom < window.pageYOffset - 40){
+    console.log(document.body.getBoundingClientRect())
+    if(/*headerElem.getBoundingClientRect().bottom < window.pageYOffset*/ window.pageYOffset !== 0){
         headerElem.classList.contains('page__header--fixed') ? null : headerElem.classList.add('page__header--fixed')
     }
     else{
         headerElem.classList.contains('page__header--fixed') ? headerElem.classList.remove('page__header--fixed') : null
         
     }
-    
-} 
+}
